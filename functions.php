818de05f8339f61c1c513864baa6a566d903bdba
Rafal Dittwald <rafal.dittwald@gmail.com>
2010-10-17 22:50:07 -0400

    added shortcodes

--- functions.php
+++ functions.php
@@ -112,4 +112,15 @@ return $existing_mimes;
  
 }
 
+
+function field_func($atts) {
+   global $post;
+   $name = $atts['name'];
+   if (empty($name)) return;
+
+   return get_post_meta($post->ID, $name, true);
+}
+
+add_shortcode('field', 'field_func');
+
 ?>
